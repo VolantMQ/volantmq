58e2e4d3d84bbb510d9c969b15214ce426c45d9b
Artur Troian <troian.ap@gmail.com>
2017-05-03 11:42:48 +0300

    Add missing register for topic mem provider
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- topics/mem/topics.go
+++ topics/mem/topics.go
@@ -43,7 +43,11 @@ type provider struct {
 	rRoot *rNode
 }
 
-//var _ provider = (topics.Provider)(nil)
+var _ topics.Provider = (*provider)(nil)
+
+func init() {
+	topics.Register("mem", NewMemProvider())
+}
 
 // NewMemProvider returns an new instance of the provider, which is implements the
 // TopicsProvider interface. provider is a hidden struct that stores the topic
@@ -225,46 +229,32 @@ func equal(k1, k2 interface{}) bool {
 	switch k1 := k1.(type) {
 	case string:
 		return k1 == k2.(string)
-
 	case int64:
 		return k1 == k2.(int64)
-
 	case int32:
 		return k1 == k2.(int32)
-
 	case int16:
 		return k1 == k2.(int16)
-
 	case int8:
 		return k1 == k2.(int8)
-
 	case int:
 		return k1 == k2.(int)
-
 	case float32:
 		return k1 == k2.(float32)
-
 	case float64:
 		return k1 == k2.(float64)
-
 	case uint:
 		return k1 == k2.(uint)
-
 	case uint8:
 		return k1 == k2.(uint8)
-
 	case uint16:
 		return k1 == k2.(uint16)
-
 	case uint32:
 		return k1 == k2.(uint32)
-
 	case uint64:
 		return k1 == k2.(uint64)
-
 	case uintptr:
 		return k1 == k2.(uintptr)
 	}
-
 	return false
 }
