7fbe418867b4f8c4d4ee91add316321677037e1c
Artur Troian <troian.ap@gmail.com>
2019-10-12 10:54:03 +0300

    ci: fix path to cmd
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

--- go.test.sh
+++ go.test.sh
@@ -5,7 +5,7 @@ echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
     go test -race -coverprofile=profile.out -covermode=atomic ${d}
-    if [ -f profile.out ]; then
+    if [[ -f profile.out ]]; then
         cat profile.out >> coverage.txt
         rm profile.out
     fi
