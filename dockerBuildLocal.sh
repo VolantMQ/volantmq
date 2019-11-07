09f1825a4e265344c409a385d191544fa0646a19
Artur Troian <troian.ap@gmail.com>
2019-11-07 11:42:18 -0500

    ci: build docker
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

---
+++ dockerBuildLocal.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+buildArgs=
+
+if [[ ! -z "$1" ]]; then
+    buildArgs="-t $1"
+fi
+
+docker build -f ./local.Dockerfile ${buildArgs} .
+
+echo "removing intermediate container"
+docker rmi -f $(docker images -q --filter label=stage=intermediate)
