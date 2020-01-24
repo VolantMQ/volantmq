df9a2dbf1b07b39b3fcec9c911ac199f521c858f
Artur Troian <troian.ap@gmail.com>
2020-01-24 10:52:10 -0500

    test: rename go.test to go.test.codeclimate
    Refactor way of gathering output from package list
    
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

---
+++ go.test.codeclimate.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+for pkg in $(go list ./... | grep -v main); do
+    go test -coverprofile=$(echo ${pkg} | tr / -).cover ${pkg}
+done
+
+echo "mode: set" > c.out
+grep -h -v "^mode:" ./*.cover >> c.out
+rm -f *.cover
